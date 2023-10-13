61ef61ffc17157c3af27457bd54329b95e38a0df
Manuel Vazquez <pon.tu.correo.aca@gmail.com>
2023-10-13 13:25:57 -0600

    adds for to execute 5 times

--- tests/test-vpn.spec.ts
+++ tests/test-vpn.spec.ts
@@ -1,13 +1,14 @@
 import {test, expect,} from '@playwright/test'
+for(let i=0;i<5;i+=1){
+    test(`test vpn ${i}`, async({ page }) => {
+        test.slow()
+        await page.goto('https://www.whatismyip.com')
 
-test('test vpn', async({ page }) => {
-    test.slow()
-    await page.goto('https://www.whatismyip.com')
+        const ipElements = await page.locator('.ip-info-line').all()
+        for(const ipElement of ipElements){
+            console.log(await ipElement.textContent())
+        }
 
-    const ipElements = await page.locator('.ip-info-line').all()
-    for(const ipElement of ipElements){
-        console.log(await ipElement.textContent())
-    }
-
-    await expect(page.locator('#geo')).toContainText('Japan')
-})
+        await expect(page.locator('#geo')).toContainText('Japan')
+    })
+}
